fba4f3b461105f160b17a907904e009115f34530
latifmd <latifmd@yahoo.com>
2020-05-21 10:09:00 +0600

    Added a new method called greeting to Hello.java

--- Hello.java
+++ Hello.java
@@ -1,5 +1,10 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vs code is Awesome ...");
+		greeting("HM Shohug");
+	}
+	
+	public static void greeting(String name){
+		System.out.println("Welcome " + name);
 	}
 }
