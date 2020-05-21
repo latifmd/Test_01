9b49180c7f250aa886ac7e1b608aeffe2dfc4849
latifmd <latifmd@yahoo.com>
2020-05-21 11:41:59 +0600

    Change greeting method

--- Hello.java
+++ Hello.java
@@ -1,10 +1,10 @@
 public class Hello {
 	public static void main(String[] args) {
 		System.out.println("Vs code is Awesome ...");
-		greeting("HM Shohug");
+		greeting("Git");
 	}
 	
 	public static void greeting(String name){
-		System.out.println("Welcome " + name);
+		System.out.println("Bye Bye " + name);
 	}
 }
